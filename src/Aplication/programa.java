39e2f45c7e8d436fd20522d6caa1288af03db578
Andrew <andrewoliveira282@gmail>
2021-10-04 20:04:57 -0300

    iniciando a class position

--- src/Aplication/programa.java
+++ src/Aplication/programa.java
@@ -1,7 +1,11 @@
 package Aplication;
 
+import BordGame.Position;
+
 public class programa {
     public static void main(String[] args) {
         System.out.println("hello word!!");
+        Position pos = new Position(3,5);
+        System.out.println(pos);
     }
 }
