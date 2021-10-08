906184e9575f013d6df1d3f38ed0903c652de941
Andrew <andrewoliveira282@gmail>
2021-10-08 11:20:53 -0300

    fazendo as peças do taboleiro

--- src/Aplication/programa.java
+++ src/Aplication/programa.java
@@ -1,10 +1,17 @@
 package Aplication;
 
 import BordGame.Board;
+import Chess.ChessMatch;
+
+
 
 
 public class programa {
     public static void main(String[] args) {
-       Board board = new Board(8,8);
+
+        //Board board = new Board(8,8);
+        ChessMatch chessMatch = new ChessMatch();
+        UI.printBoard(chessMatch.getPieces());
+
     }
 }
